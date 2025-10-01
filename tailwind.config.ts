590a1d3606f28dd86f8de16a03fbaa5b00483236
Ayush Rawal <ayushrawal2003@gmail.com>
2025-10-01 19:05:33 +0000

    change the font family of the headings and sections name

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['Playfair Display', 'serif'],
+        body: ['Lato', 'sans-serif'],
+        headline: ['Merriweather', 'serif'],
         code: ['monospace'],
       },
       colors: {
